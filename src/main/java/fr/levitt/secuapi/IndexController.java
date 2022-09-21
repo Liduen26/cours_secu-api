add4a70254a866efd1755828cabb168cff4ba974
Liduen <raphael.orieux@gmail.com>
2022-09-21 12:01:34 +0200

    premiers tests

--- src/main/java/fr/levitt/secuapi/IndexController.java
+++ src/main/java/fr/levitt/secuapi/IndexController.java
@@ -1,11 +1,31 @@
 package fr.levitt.secuapi;
 
-import fr.levitt.secuapi.core.Utilisateur;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.levitt.secuapi.core.Utilisateur;
+
 @RestController
 public class IndexController {
+
+    ArrayList<Utilisateur> list = new ArrayList();
+
+    @GetMapping("/add_user")
+    public void add_user() {
+        Utilisateur user = new Utilisateur();
+        user.setNom("hey");
+        user.setPrenom("hoy");
+        list.add(user);
+    }
+
+    @GetMapping("/get_user")
+    public Utilisateur get_user() {
+        String result = "";
+        return list.get(0);
+    }
+
 
     @GetMapping("/")
     public Utilisateur index() {
@@ -14,4 +34,6 @@ public class IndexController {
         utilisateur.setPrenom("Tata");
         return utilisateur;
     }
+
+
 }
